28e678ae73a6c04527defe514a1014f3117c8311
Wootness <noforu@gmail.com>
2018-07-03 18:56:09 -0700

    Remove unused old icons

--- PacketStudio/Properties/Resources.Designer.cs
+++ PacketStudio/Properties/Resources.Designer.cs
@@ -61,16 +61,6 @@ namespace PacketStudio.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon bytesToPcap {
-            get {
-                object obj = ResourceManager.GetObject("bytesToPcap", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap csharp {
@@ -153,29 +143,9 @@ namespace PacketStudio.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap wireshark {
-            get {
-                object obj = ResourceManager.GetObject("wireshark", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap WS {
             get {
                 object obj = ResourceManager.GetObject("WS", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap ws_dir {
-            get {
-                object obj = ResourceManager.GetObject("ws_dir", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
