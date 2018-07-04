c2ad838d16a79871570dd3734c0d6af5347b2889
Wootness <noforu@gmail.com>
2018-07-03 20:21:45 -0700

    Allow dropping in background, dock manager panels

---
+++ PacketStudio/Utils/EnumeratorExt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+namespace PacketStudio.Utils
+{
+    internal static class EnumeratorExt
+    {
+        internal static IEnumerable Iterate(this IEnumerator iterator)
+        {
+            while (iterator.MoveNext())
+                yield return iterator.Current;
+        }
+    }
+}
