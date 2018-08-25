3c64c331f34801ace55753e2c56c6a543ad014af
Wootness <noforu@gmail.com>
2018-08-25 12:46:14 +0300

    Added 'Insert ASCII' Feature

--- PacketStudio/Properties/Resources.Designer.cs
+++ PacketStudio/Properties/Resources.Designer.cs
@@ -163,6 +163,16 @@ namespace PacketStudio.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap text_box {
+            get {
+                object obj = ResourceManager.GetObject("text_box", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap WS {
             get {
                 object obj = ResourceManager.GetObject("WS", resourceCulture);
