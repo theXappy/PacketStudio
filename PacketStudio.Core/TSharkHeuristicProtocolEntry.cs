70fea3d0ec9100529abe0d13447e80aad6537025
Wootness <noforu@gmail.com>
2018-12-19 21:28:40 +0200

    Improvements to heuristics handling
    * The heuristics retrival is started at the background in start-up, less wait time when trying to open the dialog
    * Added a static list of actual heuristic dissector names (from WS sources) to compare against and, hopefully, recove where TShark's output mis-align with the actual names.

--- PacketStudio.Core/TSharkHeuristicProtocolEntry.cs
+++ PacketStudio.Core/TSharkHeuristicProtocolEntry.cs
@@ -2,16 +2,29 @@
 {
     public class TSharkHeuristicProtocolEntry
     {
-        public string ProtoName { get; private set; }
-        public string CarryingProto { get; private set; }
+        public string ProtoName { get; set; }
+        public string CarryingProto { get;  set; }
         public bool Enabled { get; private set; }
-        public string ShortName => ProtoName + "_" + CarryingProto;
+        public string ShortName => _customShortName ?? (ProtoName + "_" + CarryingProto);
+
+        private string _customShortName = null;
 
         public TSharkHeuristicProtocolEntry(string protoName, string carryingProto, bool enabled)
         {
             ProtoName = protoName;
             CarryingProto = carryingProto;
             Enabled = enabled;
+        }
+
+        public void SetCustomShortName(string name)
+        {
+            _customShortName = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Proto Name: {ProtoName}, Carrying Proto: {CarryingProto}, Abrv: {ShortName}";
+
         }
     }
 }
