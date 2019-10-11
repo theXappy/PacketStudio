73dfb24d276a4745b52cbe645f08d6be73477022
Shappy <noforu@gmail.com>
2019-10-11 13:20:26 +0300

    Last Wiresharks used - easy switching feature

--- PacketStudio/Properties/Settings.Designer.cs
+++ PacketStudio/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PacketStudio.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -140,6 +140,18 @@ namespace PacketStudio.Properties {
             }
             set {
                 this["NeedToBeDisabledDissectors"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string LastWiresharksList {
+            get {
+                return ((string)(this["LastWiresharksList"]));
+            }
+            set {
+                this["LastWiresharksList"] = value;
             }
         }
     }
